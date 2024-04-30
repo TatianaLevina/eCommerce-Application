d481f72c3b50a6896697c761de8af516397f74e2
Demetrius81 <shizimaniak@gmail.com>
2024-04-30 17:50:13 +0300

    feat: RSS-ECOMM-1_10 and  RSS-ECOMM-1_14 set up eslint RSS-ECOM-1_11 and  RSS-ECOMM-1_15 add prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'eslint-plugin-prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'prettier/prettier': [
+          'error',
+          {
+            endOfLine: 'lf',
+          },
+        ],
+        '@typescript-eslint/consistent-type-imports': 'error',
+      },
+      noInlineConfig: true,
+    },
+  ],
+};
