ecaa35f5219f00ef5d1db0f12996c0f036bfbcfe
Demetrius81 <shizimaniak@gmail.com>
2024-04-30 17:32:55 +0300

    feat: RSS-ECOMM-1_09 configure typescript

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import checker from 'vite-plugin-checker';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vite';
+
+export default defineConfig(({ command, mode }) => {
+  if (command === 'build' && mode === 'production') {
+    return {
+      base: './',
+      plugins: [checker({ typescript: true }), tsconfigPaths()],
+    };
+  } else {
+    return {
+      plugins: [checker({ typescript: true }), tsconfigPaths()],
+    };
+  }
+});
