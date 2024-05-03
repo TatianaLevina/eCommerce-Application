60101cd3bbc6f511ea87c845e2969c3b43497fb7
Demetrius81 <shizimaniak@gmail.com>
2024-05-03 09:51:07 +0300

    fix: remove default favicon and comment line

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
-// import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig(({ command, mode }) => {
   if (command === 'build' && mode === 'production') {
@@ -12,7 +11,6 @@ export default defineConfig(({ command, mode }) => {
   } else {
     return {
       plugins: [checker({ typescript: true }), tsconfigPaths()],
-      // plugins: [checker({ typescript: true }), tsconfigPaths(), svelte({ hot: !process.env.VITEST })],
       test: {
         globals: true,
         environment: 'jsdom',
