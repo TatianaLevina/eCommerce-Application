f63729634f7141d7c5af785b8193c9c4a3c00372
Demetrius81 <shizimaniak@gmail.com>
2024-05-05 22:52:05 +0300

    refactor: change regexp to branch name validate

--- .validate-branch-namerc.cjs
+++ .validate-branch-namerc.cjs
@@ -1,4 +1,4 @@
 module.exports = {
-  pattern: /^[a-z0-9\/-]+\/[a-z0-9]+\/RSS-ECOMM-\d{1,2}_\d{2}((-\d{1,2}_\d{2})?|(,\d{1,2}_\d{2})*)-[a-z-]+/,
+  pattern: /(^[a-z0-9\/-]+\/[a-z0-9]+\/RSS-ECOMM-\d{1,2}_\d{2}((-\d{1,2}_\d{2})?|(,\d{1,2}_\d{2})*)-[a-z-]+)|(^release\/[a-z-]+)/,
   errorMsg: 'Branch name is wrong format.',
 };
