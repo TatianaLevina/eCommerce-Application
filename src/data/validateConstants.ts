e99373813f4a46b40f3726c2097c42667933d83b
Dzmitry Ryzhou <95518976+Demetrius81@users.noreply.github.com>
2024-05-14 11:06:48 +0300

    refactor: change validation rules

--- src/data/validateConstants.ts
+++ src/data/validateConstants.ts
@@ -1,7 +1,17 @@
+import type { Rule } from 'antd/es/form';
+
 const validateConstant = {
   namePattern: /^(?!\s+)[a-zA-Z]+(?!\s+)$/,
   emailPattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
   passwordPattern: /^(?!\s+)(?=.*[0-9])(?=.*[!@#$%^&*.])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*.]{8,}(?!\s+)$/,
+  passwordRules: [
+    { required: true, message: 'Please input your password!' },
+    { pattern: /(?=.*[A-Z]).*/, message: 'Password must contain at least 1 uppercase letter.' },
+    { pattern: /(?=.*[A-Z]).*/, message: 'Password must contain at least 1 lowercase letter.' },
+    { pattern: /(?=.*\d).*/, message: 'Password must contain at least 1 digit.' },
+    { pattern: /(?=.*[^\w\s]|_).*/, message: 'Password must contain at least 1 special character.' },
+    { min: 8, message: 'Password length must be minimum 8 characters.' },
+  ],
   AgeLimit: 13,
   dateFormat: 'YYYY-MM-DD',
   streetPattern: /^(?!\s+).+(?!\s+)$/,
@@ -13,6 +23,7 @@ export type ValidateConstant = {
   namePattern: RegExp;
   emailPattern: RegExp;
   passwordPattern: RegExp;
+  passwordRules: Rule[];
   AgeLimit: number;
   dateFormat: string;
   streetPattern: RegExp;
