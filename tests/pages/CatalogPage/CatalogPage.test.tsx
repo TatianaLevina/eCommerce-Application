922f91ebd2f45a702ac172f55b3d713199d5f86d
Tatiana Levina <tatiana.levina.v@gmail.com>
2024-05-15 11:56:16 +0300

    feat: add simple test for catalog page

---
+++ tests/pages/CatalogPage/CatalogPage.test.tsx
@@ -0,0 +1,56 @@
+import { render, act } from '@testing-library/react';
+
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from '@contexts/AuthContext';
+import CatalogPage from '@/pages/CatalogPage/CatalogPage';
+
+describe('CatalogPage tests', () => {
+  describe('Render element', () => {
+    let container: HTMLElement | null = null;
+
+    beforeEach(() => {
+      container = document.createElement('div');
+      container.id = 'div_root';
+      document.body.append(container);
+    });
+
+    afterEach(() => {
+      container?.remove();
+      container = null;
+    });
+
+    test('Should ErrorPage be defined', () => {
+      act(() => {
+        render(
+          <AuthProvider>
+            <CatalogPage />
+          </AuthProvider>,
+          {
+            wrapper: BrowserRouter,
+            container: container!,
+          },
+        );
+      });
+
+      const el = container?.querySelector('div');
+      expect(el).toBeDefined();
+    });
+
+    test('Contains HTMLElement', () => {
+      act(() => {
+        render(
+          <AuthProvider>
+            <CatalogPage />
+          </AuthProvider>,
+          {
+            wrapper: BrowserRouter,
+            container: container!,
+          },
+        );
+      });
+
+      const el = container?.querySelector('div');
+      expect(el).toBeInstanceOf(HTMLElement);
+    });
+  });
+});
