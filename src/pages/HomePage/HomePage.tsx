0a64c49adb1bea76ed57cde41ad1363653bd31f8
Tatiana Levina <tatiana.levina.v@gmail.com>
2024-05-15 23:06:35 +0300

    fix: style for adaptive behavior login page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,19 +1,17 @@
 import type React from 'react';
-import { Typography } from 'antd';
+// import { Typography } from 'antd';
 import { Carousel, Flex } from 'antd';
 import IMAGES from '@/images/Images';
 
 const HomePage: React.FC = () => {
-  const { Title } = Typography;
+  // const { Title } = Typography;
   const onChange = (currentSlide: number) => {
     console.log(currentSlide);
   };
   return (
     <>
       <Flex vertical align="center">
-        <Title level={2} className="custom-title">
-          Welcome to our Sweet Home
-        </Title>
+        <h1 className="custom-title">Welcome to our Sweet Home</h1>
       </Flex>
       <Carousel autoplay afterChange={onChange}>
         <div>
