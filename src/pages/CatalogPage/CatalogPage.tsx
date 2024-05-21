fc4784ac263b71ffc943422c48a86257d0786c0a
ynadt <2769286@gmail.com>
2024-05-21 12:32:25 +0300

    feat: add getSingleProductService

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,9 +1,11 @@
 import { Spin } from 'antd';
 import { useCategory } from '@contexts/CategoriesContext.tsx';
+import { getSingleProductService } from '@services/ProductsServise.ts';
 
 export default function CatalogPage() {
   const { categories, loading } = useCategory();
-
+  // f2b931a3-3ff1-4505-8fec-e67a719125b5 is a test id of a product
+  console.log(getSingleProductService('f2b931a3-3ff1-4505-8fec-e67a719125b5'));
   return (
     <div>
       <h1 className="custom-title">Catalog Page</h1>
