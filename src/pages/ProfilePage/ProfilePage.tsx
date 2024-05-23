c8edc1fda7756e8a1cc9522c1b1468b3c2bccd2c
Tatiana Levina <tatiana.levina.v@gmail.com>
2024-05-24 01:47:47 +0300

    feat: add checkbox for addresses, add style

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,5 +1,5 @@
 import type React from 'react';
-import { Button, DatePicker, Flex, Form, Input, Select, Typography } from 'antd';
+import { Button, Checkbox, DatePicker, Flex, Form, Input, Select, Typography } from 'antd';
 import { useAuth } from '@/contexts/AuthContext';
 import validateConstant from '@data/validateConstants';
 import moment from 'moment';
@@ -18,6 +18,7 @@ interface UserAddress {
   streetName?: string;
   city?: string;
   postalCode?: string;
+  isDefault?: boolean;
 }
 
 const ProfilePage: React.FC = () => {
@@ -39,12 +40,14 @@ const ProfilePage: React.FC = () => {
     streetName: '',
     city: '',
     postalCode: '',
+    isDefault: true,
   };
   const initShippingAddress: UserAddress = {
     country: '',
     streetName: '',
     city: '',
     postalCode: '',
+    isDefault: false,
   };
 
   if (user?.addresses[0].streetName) {
@@ -58,6 +61,16 @@ const ProfilePage: React.FC = () => {
   }
   if (user?.addresses[0].postalCode) {
     initBillingAddress.postalCode = user?.addresses[0].postalCode;
+  }
+  if (user?.defaultBillingAddressId) {
+    initBillingAddress.isDefault = true;
+  } else {
+    initBillingAddress.isDefault = false;
+  }
+  if (user?.defaultShippingAddressId) {
+    initShippingAddress.isDefault = true;
+  } else {
+    initShippingAddress.isDefault = false;
   }
   if (user?.addresses.length === 1) {
     initShippingAddress.streetName = initBillingAddress.streetName;
@@ -80,146 +93,151 @@ const ProfilePage: React.FC = () => {
   }
 
   return (
-    <Flex vertical align="flex-start">
-      {/* <h1 className="custom-title">Profile</h1> */}
-      {editMode ? (
-        <>
-          <Button
-            style={{ alignSelf: 'flex-end' }}
-            type="primary"
-            className={' primary-custom-color'}
-            onClick={() => setEditMode(!editMode)}
-          >
-            Cancel
-          </Button>
-        </>
-      ) : (
-        <>
-          {' '}
-          <Button
-            style={{ alignSelf: 'flex-end' }}
-            type="primary"
-            className={' primary-custom-color'}
-            onClick={() => setEditMode(!editMode)}
-          >
-            Edit
-          </Button>{' '}
-        </>
-      )}
-
-      <Form
-        form={form}
-        name="profile"
-        onFinish={onFinish}
-        initialValues={{
-          firstName: initValuesGeneralInfo.firstName,
-          lastName: initValuesGeneralInfo.lastName,
-          billingStreet: initBillingAddress.streetName,
-          billingCity: initBillingAddress.city,
-          billingPostalCode: initBillingAddress.postalCode,
-          shippingStreet: initShippingAddress.streetName,
-          shippingCity: initShippingAddress.city,
-          shippingPostalCode: initShippingAddress.postalCode,
-        }}
-        onValuesChange={onFormLayoutChange}
-        disabled={!editMode}
-      >
-        <div className="profile__general-info">
-          <Title
-            level={4}
-            color="#376a4f"
-            style={{
-              color: '#376a4f',
-              textAlign: 'left',
-              marginTop: 10,
-            }}
-          >
-            General Information
-          </Title>
-          <Form.Item name="firstName" label="First Name">
-            <Input />
-          </Form.Item>
-          <Form.Item name="lastName" label="Last Name">
-            <Input />
-          </Form.Item>
-          <Form.Item name="birthDate" label="Birth date">
-            <DatePicker
-              defaultValue={moment(initValuesGeneralInfo.birthDate, validateConstant.dateFormat)}
-              format={validateConstant.dateFormat}
-            />
-          </Form.Item>
-        </div>
-        <Title
-          level={4}
-          style={{
-            color: '#376a4f',
-            textAlign: 'left',
-            marginTop: 10,
+    <>
+      <h1 className="custom-title">Profile</h1>
+      <Flex justify="center" style={{ width: '100%' }}>
+        <Form
+          form={form}
+          name="profile"
+          onFinish={onFinish}
+          initialValues={{
+            firstName: initValuesGeneralInfo.firstName,
+            lastName: initValuesGeneralInfo.lastName,
+            billingStreet: initBillingAddress.streetName,
+            billingCity: initBillingAddress.city,
+            billingPostalCode: initBillingAddress.postalCode,
+            shippingStreet: initShippingAddress.streetName,
+            shippingCity: initShippingAddress.city,
+            shippingPostalCode: initShippingAddress.postalCode,
           }}
+          onValuesChange={onFormLayoutChange}
+          disabled={!editMode}
         >
-          Addresses
-        </Title>
-        <Flex gap="large" justify="space-between" style={{ width: '100%' }}>
-          <div>
+          <div className="profile__general-info">
             <Title
-              level={5}
+              level={4}
+              color="#376a4f"
               style={{
                 color: '#376a4f',
                 textAlign: 'left',
                 marginTop: 10,
               }}
             >
-              Billing Address
+              General Information
             </Title>
-            <Form.Item name="billingStreet" label="Street">
+            <Form.Item name="firstName" label="First Name">
               <Input />
             </Form.Item>
-            <Form.Item name="billingCity" label="City">
+            <Form.Item name="lastName" label="Last Name">
               <Input />
             </Form.Item>
-            <Form.Item name="billingCountry" label="Country">
-              <Select defaultValue={initBillingAddress.country} options={options} placeholder="Select country" />
-            </Form.Item>
-            <Form.Item name="billingPostalCode" label="Postal Code">
-              <Input />
+            <Form.Item name="birthDate" label="Birth date">
+              <DatePicker
+                defaultValue={moment(initValuesGeneralInfo.birthDate, validateConstant.dateFormat)}
+                format={validateConstant.dateFormat}
+              />
             </Form.Item>
           </div>
-          <div>
+          <div className="profile__addresses">
             <Title
-              level={5}
+              level={4}
               style={{
                 color: '#376a4f',
                 textAlign: 'left',
                 marginTop: 10,
               }}
             >
-              Shipping Address
+              Addresses
             </Title>
-            <Form.Item name="shippingStreet" label="Street">
-              <Input />
-            </Form.Item>
-            <Form.Item name="shippingCity" label="City">
-              <Input />
-            </Form.Item>
-            <Form.Item name="shippingCountry" label="Country">
-              <Select defaultValue={initShippingAddress.country} options={options} placeholder="Select country" />
-            </Form.Item>
-            <Form.Item name="shippingPostalCode" label="Postal Code">
-              <Input />
-            </Form.Item>
+            <Flex gap="large" wrap>
+              <div>
+                <Title
+                  level={5}
+                  style={{
+                    color: '#376a4f',
+                    textAlign: 'left',
+                    marginTop: 10,
+                  }}
+                >
+                  Billing Address
+                </Title>
+                <Form.Item name="billingStreet" label="Street">
+                  <Input />
+                </Form.Item>
+                <Form.Item name="billingCity" label="City">
+                  <Input />
+                </Form.Item>
+                <Form.Item name="billingCountry" label="Country">
+                  <Select defaultValue={initBillingAddress.country} options={options} placeholder="Select country" />
+                </Form.Item>
+                <Form.Item name="billingPostalCode" label="Postal Code">
+                  <Input />
+                </Form.Item>
+                <Checkbox checked={initBillingAddress.isDefault}> Billing address as default</Checkbox>
+              </div>
+              <div>
+                <Title
+                  level={5}
+                  style={{
+                    color: '#376a4f',
+                    textAlign: 'left',
+                    marginTop: 10,
+                  }}
+                >
+                  Shipping Address
+                </Title>
+                <Form.Item name="shippingStreet" label="Street">
+                  <Input />
+                </Form.Item>
+                <Form.Item name="shippingCity" label="City">
+                  <Input />
+                </Form.Item>
+                <Form.Item name="shippingCountry" label="Country">
+                  <Select defaultValue={initShippingAddress.country} options={options} placeholder="Select country" />
+                </Form.Item>
+                <Form.Item name="shippingPostalCode" label="Postal Code">
+                  <Input />
+                </Form.Item>
+                <Checkbox checked={initShippingAddress.isDefault}> Shipping address as default</Checkbox>
+              </div>
+            </Flex>
           </div>
-        </Flex>
+          {editMode ? (
+            <>
+              <Button type="primary" className={'primary-custom-color'} htmlType="submit">
+                Update
+              </Button>{' '}
+            </>
+          ) : (
+            <> </>
+          )}
+        </Form>
         {editMode ? (
           <>
-            <Button type="primary" className={'primary-custom-color'} htmlType="submit">
-              Update
-            </Button>{' '}
+            <Button
+              style={{ alignSelf: 'flex-start' }}
+              type="primary"
+              className={' primary-custom-color'}
+              onClick={() => setEditMode(!editMode)}
+            >
+              Cancel
+            </Button>
           </>
         ) : (
-          <> </>
+          <>
+            {' '}
+            <Button
+              style={{ alignSelf: 'flex-start' }}
+              type="primary"
+              className={' primary-custom-color'}
+              onClick={() => setEditMode(!editMode)}
+            >
+              Edit
+            </Button>{' '}
+          </>
         )}
-      </Form>
-    </Flex>
+      </Flex>
+    </>
   );
 };
 export default ProfilePage;
