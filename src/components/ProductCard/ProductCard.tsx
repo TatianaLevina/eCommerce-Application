dd0f409ce9b10300a132015c95cceded3268ee13
Dzmitry Ryzhou <95518976+Demetrius81@users.noreply.github.com>
2024-06-06 09:14:57 +0300

    issue RSS-ECOMM-4_25 Add cart template (#135)
    * feat: add preloader
    
    * feat: add cart page template and add cart item
    
    * feat: set product type
    
    * feat: style elements
    
    * refactor: refactor the code
    
    * fix: utility naming

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -3,6 +3,7 @@ import { Card } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import type { ProductProjection } from '@commercetools/platform-sdk';
 import '@components/ProductCard/ProductCard.scss';
+import ImageCustom from '../ImageCustom/ImageCustom';
 
 interface ProductCardProps {
   product: ProductProjection;
@@ -36,7 +37,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product, categorySlug, format
         </div>
       )}
       <div className="product-card__custom-image">
-        <img className="product-card__img" src={imageUrl} alt={product.name['en-US']} />
+        <ImageCustom className="product-card__img" src={imageUrl} alt={product.name['en-US']}></ImageCustom>
       </div>
       <div className="product-card__details">
         <p className={`product-card__price ${discountedPrice ? 'product-card__price_discounted' : ''}`}>
--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,7 +1,352 @@
-export default function CartPage() {
+import { useState } from 'react';
+import { Button } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import { type Cart } from '@commercetools/platform-sdk';
+import CartItem from '@components/CartItem/CartItem';
+import '@pages/CartPage/CartPage.scss';
+import { createCartService, getCartService } from '@/services/CartService';
+
+const cart: Cart = {
+  id: 'string',
+  version: 1,
+  key: 'string',
+  customerId: 'string',
+  customerEmail: 'string',
+  anonymousId: 'string',
+  lineItems: [
+    {
+      id: '00000000-5b3c-487e-8ba0-1305ad7d0da0',
+      productId: '5d9577f9-5b3c-487e-8ba0-1305ad7d0da0',
+      name: {
+        'en-US': 'Portoy',
+      },
+      productSlug: {
+        'en-US': 'armchair-portoy',
+      },
+      productType: {
+        typeId: 'product-type',
+        id: 'string',
+      },
+      variant: {
+        attributes: [
+          {
+            name: 'designer',
+            value: 'Middle Earth Chemical Plant',
+          },
+          {
+            name: 'material',
+            value: {
+              key: 'plastic',
+              label: {
+                'en-US': 'Plastic',
+              },
+            },
+          },
+          {
+            name: 'arrangement',
+            value: {
+              key: 'house',
+              label: {
+                'en-US': 'house',
+              },
+            },
+          },
+        ],
+        assets: [],
+        images: [
+          {
+            url: 'https://images.cdn.europe-west1.gcp.commercetools.com/ff91879f-f4a7-436e-bc45-97970b384e8e/photo_2024-05-24_15--r-Q3wjA0.jpg',
+            dimensions: {
+              w: 0,
+              h: 0,
+            },
+          },
+          {
+            url: 'https://images.cdn.europe-west1.gcp.commercetools.com/ff91879f-f4a7-436e-bc45-97970b384e8e/photo_2024-05-24_15--RlxEMgWo.jpg',
+            dimensions: {
+              w: 0,
+              h: 0,
+            },
+          },
+          {
+            url: 'https://images.cdn.europe-west1.gcp.commercetools.com/ff91879f-f4a7-436e-bc45-97970b384e8e/photo_2024-05-24_15--hCwt9BRg.jpg',
+            dimensions: {
+              w: 0,
+              h: 0,
+            },
+          },
+        ],
+        prices: [
+          {
+            id: '12cdf84b-640c-4088-9880-9f840d7f692c',
+            value: {
+              type: 'centPrecision',
+              currencyCode: 'USD',
+              centAmount: 9999,
+              fractionDigits: 2,
+            },
+          },
+        ],
+        key: '17',
+        sku: '17',
+        id: 1,
+      },
+      price: {
+        id: 'string',
+        value: {
+          type: 'centPrecision',
+          centAmount: 100,
+          currencyCode: 'USD',
+          fractionDigits: 1,
+        },
+      },
+      quantity: 5,
+      totalPrice: {
+        type: 'centPrecision',
+        centAmount: 100,
+        currencyCode: 'USD',
+        fractionDigits: 1,
+      },
+      discountedPricePerQuantity: [],
+      taxedPricePortions: [],
+      state: [],
+      perMethodTaxRate: [],
+      priceMode: 'LineItemPriceMode',
+      lineItemMode: 'LineItemMode',
+    },
+    {
+      id: '11111111-5b3c-487e-8ba0-1305ad7d0da0',
+      productId: '5d9577f9-5b3c-487e-8ba0-1305ad7d0da0',
+      name: {
+        'en-US': 'Portoy',
+      },
+      productSlug: {
+        'en-US': 'armchair-portoy',
+      },
+      productType: {
+        typeId: 'product-type',
+        id: 'string',
+      },
+      variant: {
+        attributes: [
+          {
+            name: 'designer',
+            value: 'Middle Earth Chemical Plant',
+          },
+          {
+            name: 'material',
+            value: {
+              key: 'plastic',
+              label: {
+                'en-US': 'Plastic',
+              },
+            },
+          },
+          {
+            name: 'arrangement',
+            value: {
+              key: 'house',
+              label: {
+                'en-US': 'house',
+              },
+            },
+          },
+        ],
+        assets: [],
+        images: [
+          {
+            url: 'https://images.cdn.europe-west1.gcp.commercetools.com/ff91879f-f4a7-436e-bc45-97970b384e8e/photo_2024-05-24_15--r-Q3wjA0.jpg',
+            dimensions: {
+              w: 0,
+              h: 0,
+            },
+          },
+          {
+            url: 'https://images.cdn.europe-west1.gcp.commercetools.com/ff91879f-f4a7-436e-bc45-97970b384e8e/photo_2024-05-24_15--RlxEMgWo.jpg',
+            dimensions: {
+              w: 0,
+              h: 0,
+            },
+          },
+          {
+            url: 'https://images.cdn.europe-west1.gcp.commercetools.com/ff91879f-f4a7-436e-bc45-97970b384e8e/photo_2024-05-24_15--hCwt9BRg.jpg',
+            dimensions: {
+              w: 0,
+              h: 0,
+            },
+          },
+        ],
+        prices: [
+          {
+            id: '12cdf84b-640c-4088-9880-9f840d7f692c',
+            value: {
+              type: 'centPrecision',
+              currencyCode: 'USD',
+              centAmount: 9999,
+              fractionDigits: 2,
+            },
+          },
+        ],
+        key: '17',
+        sku: '17',
+        id: 1,
+      },
+      price: {
+        id: 'string',
+        value: {
+          type: 'centPrecision',
+          centAmount: 100,
+          currencyCode: 'USD',
+          fractionDigits: 1,
+        },
+      },
+      quantity: 5,
+      totalPrice: {
+        type: 'centPrecision',
+        centAmount: 100,
+        currencyCode: 'USD',
+        fractionDigits: 1,
+      },
+      discountedPricePerQuantity: [],
+      taxedPricePortions: [],
+      state: [],
+      perMethodTaxRate: [],
+      priceMode: 'LineItemPriceMode',
+      lineItemMode: 'LineItemMode',
+    },
+  ],
+  customLineItems: [],
+  totalLineItemQuantity: 4,
+  totalPrice: {
+    type: 'centPrecision',
+    centAmount: 100,
+    currencyCode: 'USD',
+    fractionDigits: 1,
+  },
+  discountOnTotalPrice: {
+    discountedAmount: {
+      type: 'centPrecision',
+      centAmount: 100,
+      currencyCode: 'USD',
+      fractionDigits: 1,
+    },
+    includedDiscounts: [],
+    discountedNetAmount: {
+      type: 'centPrecision',
+      centAmount: 100,
+      currencyCode: 'USD',
+      fractionDigits: 1,
+    },
+    discountedGrossAmount: {
+      type: 'centPrecision',
+      centAmount: 100,
+      currencyCode: 'USD',
+      fractionDigits: 1,
+    },
+  },
+  taxMode: 'TaxMode',
+  taxRoundingMode: 'RoundingMode',
+  taxCalculationMode: 'TaxCalculationMode',
+  inventoryMode: 'InventoryMode',
+  cartState: 'CartState',
+  shippingMode: 'ShippingMode',
+  shipping: [],
+  itemShippingAddresses: [],
+  discountCodes: [],
+  directDiscounts: [],
+  refusedGifts: [],
+  origin: 'CartOrigin',
+  createdAt: 'string',
+  lastModifiedAt: 'string',
+};
+
+function CartPage() {
+  const navigate = useNavigate();
+  const [promocode, setPromocode] = useState<string | null>(null);
+  // const [prod, setProd] = useState<ProductProjection>(product);
+
+  const enterPromoClickHandler = () => {
+    console.log('EnterPromo');
+    setPromocode('PromocodeIsActive');
+  };
+
+  const cleanClickHandler = () => {
+    console.log('cleanClickHandler');
+  };
+
+  const toCatalogClickHandler = () => {
+    navigate(`/catalog/`);
+  };
+
+  const buyClickHandler = () => {
+    console.log('buyClickHandler');
+  };
+
+  const removeClickHandler = (id: string) => {
+    console.log('REMOVE ITEM: ', id);
+    createCartService(id, 'USD');
+  };
+
+  const inputChangeHandler = (id: string, value: 1 | 99 | null) => {
+    if (!value) {
+      console.log('value is null, return');
+      return;
+    }
+    getCartService(id).then((res) => console.log(res));
+    console.log(value, ' >>> ', id);
+  };
+
   return (
-    <div>
-      <h1 className="custom-title">Cart</h1>
+    <div className="cart">
+      <div className="cart__wrapper">
+        <h1 className="custom-title">Cart</h1>
+        <div className="cart__products-box">
+          {/*>>>>>*/}
+
+          {cart.lineItems.map((item) => (
+            <CartItem
+              key={item.id}
+              product={item}
+              removeClickHandler={removeClickHandler}
+              inputChangeHandler={inputChangeHandler}
+            ></CartItem>
+          ))}
+
+          {/*<<<<<*/}
+        </div>
+        <div className="cart__bottom-box">
+          {promocode ? (
+            <p className="base-text cart__promocode">Promo info</p>
+          ) : (
+            <p className="base-text cart__promocode">Enter promocode here</p>
+          )}
+          {promocode ? (
+            <p className="cart__total base-text">Total price with Promocode: 10.00 USD</p>
+          ) : (
+            <Button className="custom-color cart__promocode" onClick={enterPromoClickHandler}>
+              Enter Promo
+            </Button>
+          )}
+        </div>
+        <div className="cart__bottom-box">
+          <div className="cart__bottom-part">
+            <Button danger onClick={cleanClickHandler}>
+              Clean Cart
+            </Button>
+            <Button className="primary-custom-color" onClick={toCatalogClickHandler}>
+              To Catalog
+            </Button>
+          </div>
+          <div className="cart__bottom-part">
+            <p className="cart__total base-text">Total Price: 100 000.00 USD</p>
+            <Button className="primary-custom-color" onClick={buyClickHandler}>
+              Buy!
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
+export default CartPage;
