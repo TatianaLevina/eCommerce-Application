0f43bfa54947ffc5e74c6e75a872b2b6cba74ac1
ynadt <2769286@gmail.com>
2024-06-07 12:05:44 +0300

    feat: remove try-catch from CartService

--- src/services/CartService.ts
+++ src/services/CartService.ts
@@ -2,55 +2,40 @@ import { createAuthFlow } from '@services/ClientBuilder.ts';
 import type { Cart, MyCartUpdateAction } from '@commercetools/platform-sdk';
 
 export const createCartService = async (currency: string): Promise<Cart | null> => {
-  try {
-    const responseCart = await createAuthFlow()
-      .me()
-      .carts()
-      .post({
-        body: {
-          currency: currency,
-        },
-      })
-      .execute();
-    if (responseCart.statusCode === 201) {
-      return responseCart.body;
-    }
-    return null;
-  } catch (e) {
-    console.error('createCartService', e);
-    return null;
+  const responseCart = await createAuthFlow()
+    .me()
+    .carts()
+    .post({
+      body: {
+        currency: currency,
+      },
+    })
+    .execute();
+  if (responseCart.statusCode === 201) {
+    return responseCart.body;
   }
+  return null;
 };
 
 export const getCartService = async (): Promise<Cart | null> => {
-  try {
-    const responseCart = await createAuthFlow().me().activeCart().get().execute();
-    if (responseCart.statusCode === 200) {
-      return responseCart.body;
-    }
-    return null;
-  } catch (e) {
-    console.error('getCartService', e);
-    return null;
+  const responseCart = await createAuthFlow().me().activeCart().get().execute();
+  if (responseCart.statusCode === 200) {
+    return responseCart.body;
   }
+  return null;
 };
 
 export const deleteCartService = async (cartId: string, version: number): Promise<Cart | null> => {
-  try {
-    const responseCart = await createAuthFlow()
-      .me()
-      .carts()
-      .withId({ ID: cartId })
-      .delete({ queryArgs: { version } })
-      .execute();
-    if (responseCart.statusCode === 200) {
-      return responseCart.body;
-    }
-    return null;
-  } catch (e) {
-    console.error('deleteCartService', e);
-    return null;
+  const responseCart = await createAuthFlow()
+    .me()
+    .carts()
+    .withId({ ID: cartId })
+    .delete({ queryArgs: { version } })
+    .execute();
+  if (responseCart.statusCode === 200) {
+    return responseCart.body;
   }
+  return null;
 };
 
 type cartUpdateActions = 'addLineItem' | 'removeLineItem' | 'addDiscountCode' | 'removeDiscountCode'; // DO WE NEED SMTH ELSE?
