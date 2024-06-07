aea95cdd879342f3177b415a09f517d23f0993f2
Tatiana Levina <tatiana.levina.v@gmail.com>
2024-06-07 16:31:13 +0300

    feat: add banner with promo codes to home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,28 +2,48 @@ import type React from 'react';
 // import { Typography } from 'antd';
 import { Carousel } from 'antd';
 import IMAGES from '@/images/Images';
+import DiscountBanner from '@/components/DiscountBanner/DiscountBanner';
+import { useDiscounts } from '@/contexts/DiscountsContext';
+import type { DiscountCode } from '@commercetools/platform-sdk';
+import type { ReactElement } from 'react';
 
 const HomePage: React.FC = () => {
-  // const { Title } = Typography;
-  const onChange = (currentSlide: number) => {
-    console.log(currentSlide);
-  };
+  const { discountCodes } = useDiscounts();
+
+  const banners: ReactElement[] = [
+    //   <>
+    //     <div className="home__img-wrapper">
+    //       <img src={IMAGES.image1} className="custom-img" alt="Sweet Home" />
+    //     </div>
+    //   </>,
+    //   <>
+    //     <div className="home__img-wrapper">
+    //       <img src={IMAGES.image2} className="custom-img" alt="Sweet Home" />
+    //     </div>
+    //   </>,
+    //   <>
+    //     <div className="home__img-wrapper">
+    //       <img src={IMAGES.image3} className="custom-img" alt="Sweet Home" />
+    //     </div>
+    //   </>,
+    //   <>
+    //     <div className="home__img-wrapper">
+    //       <img src={IMAGES.image3} className="custom-img" alt="Sweet Home" />
+    //     </div>
+    //   </>,
+  ];
+  discountCodes.forEach((code: DiscountCode, idx: number) => {
+    banners.push(
+      <>
+        <DiscountBanner discountCode={code} image={IMAGES[idx]} />
+      </>,
+    );
+  });
   return (
     <>
       <h1 className="custom-title">Welcome to our Sweet Home</h1>
-      <Carousel autoplay afterChange={onChange}>
-        <div>
-          <img src={IMAGES.image1} className="custom-img" alt="Sweet Home" />
-        </div>
-        <div>
-          <img src={IMAGES.image2} className="custom-img" alt="Sweet Home" />
-        </div>
-        <div>
-          <img src={IMAGES.image3} className="custom-img" alt="Sweet Home" />
-        </div>
-        <div>
-          <img src={IMAGES.image3} className="custom-img" alt="Sweet Home" />
-        </div>
+      <Carousel draggable arrows>
+        {...banners}
       </Carousel>
     </>
   );
