99f26c82cc03a8f34dcef400738f8f4bb5179090
Dzmitry Ryzhou <95518976+Demetrius81@users.noreply.github.com>
2024-06-11 17:34:49 +0300

    fix: fix tests

--- tests/components/ProductCard/ProductCard.test.tsx
+++ tests/components/ProductCard/ProductCard.test.tsx
@@ -2,6 +2,7 @@ import { AuthProvider } from '@contexts/AuthContext';
 import { render, act } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import ProductCard from '@components/ProductCard/ProductCard';
+import { CartProvider } from '@/contexts/CartContext';
 
 describe('ProductCard component tests', () => {
   describe('Render element', () => {
@@ -22,36 +23,38 @@ describe('ProductCard component tests', () => {
       act(() => {
         render(
           <AuthProvider>
-            <ProductCard
-              product={{
-                id: 'string',
-                version: 5,
-                createdAt: 'string',
-                lastModifiedAt: 'string',
-                productType: {
-                  typeId: 'product-type',
+            <CartProvider>
+              <ProductCard
+                product={{
                   id: 'string',
-                },
-                name: { 'en-US': 'TestProduct' },
-                slug: { 'en-US': 'Test' },
-                categories: [
-                  {
-                    typeId: 'category',
+                  version: 5,
+                  createdAt: 'string',
+                  lastModifiedAt: 'string',
+                  productType: {
+                    typeId: 'product-type',
                     id: 'string',
                   },
-                ],
-                masterVariant: {
-                  id: 2,
-                },
-                variants: [
-                  {
+                  name: { 'en-US': 'TestProduct' },
+                  slug: { 'en-US': 'Test' },
+                  categories: [
+                    {
+                      typeId: 'category',
+                      id: 'string',
+                    },
+                  ],
+                  masterVariant: {
                     id: 2,
                   },
-                ],
-              }}
-              categorySlug={'test'}
-              formatPrice={(x) => x.toFixed(2)}
-            />
+                  variants: [
+                    {
+                      id: 2,
+                    },
+                  ],
+                }}
+                categorySlug={'test'}
+                formatPrice={(x) => x.toFixed(2)}
+              />
+            </CartProvider>
           </AuthProvider>,
           {
             wrapper: BrowserRouter,
