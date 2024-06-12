4af8de566b3d9edb21a79fb497cbcf22ac3bb199
Tatiana Levina <tatiana.levina.v@gmail.com>
2024-06-12 23:28:27 +0300

    feat: add checking multiply applying promo code

--- src/components/PromoCode/PromoCode.tsx
+++ src/components/PromoCode/PromoCode.tsx
@@ -1,5 +1,7 @@
 import { useCart } from '@/contexts/CartContext';
+import { useDiscounts } from '@/contexts/DiscountsContext';
 import { checkDiscountCodeExists } from '@/services/DiscountsService';
+import type { DiscountCode, DiscountCodeInfo } from '@commercetools/platform-sdk';
 import { Button, Form, Input, notification } from 'antd';
 import type React from 'react';
 import { useState } from 'react';
@@ -18,12 +20,14 @@ const PromoCode: React.FC = () => {
       duration: 5,
     });
   };
-  const { addDiscountCode } = useCart();
+  const { discountCodes } = useDiscounts();
+  const { state, addDiscountCode } = useCart();
 
   const onFinish = async ({ promoCode }: PromoCodeFormValues) => {
-    if (!promoCode) {
+    if (!promoCode || state.cart === null) {
       return;
     }
+
     setDisabled(true);
     const isExist = await checkDiscountCodeExists(promoCode);
     form.resetFields();
@@ -32,22 +36,26 @@ const PromoCode: React.FC = () => {
       showError(`Unknown PROMO CODE: ${promoCode}`);
       return;
     }
+    const { id: discountCodeId } = discountCodes.find((code: DiscountCode) => {
+      return code.code === promoCode;
+    }) as DiscountCode;
+
+    if (
+      state.cart.discountCodes.findIndex(
+        (codeInfo: DiscountCodeInfo) => codeInfo.discountCode.id === discountCodeId,
+      ) !== -1
+    ) {
+      showError(`PROMO CODE: ${promoCode} is already applied`);
+      return;
+    }
+
     await addDiscountCode(promoCode);
   };
 
-  const onFinishFailed = (): void => {
-    showError('Fill in required fields!');
-  };
   return (
     <>
       {contextHolder}
-      <Form
-        form={form}
-        className="cart__bottom-box"
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        disabled={isDisabled}
-      >
+      <Form form={form} className="cart__bottom-box" onFinish={onFinish} disabled={isDisabled}>
         <Form.Item name="promoCode" style={{ marginBottom: '0' }}>
           <Input placeholder="Enter PROMO CODE here" />
         </Form.Item>
