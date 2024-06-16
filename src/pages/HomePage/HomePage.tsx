af2180bdb8afaaac93730823cef4b12949ee4068
Dzmitry Ryzhou <95518976+Demetrius81@users.noreply.github.com>
2024-06-16 21:29:22 +0300

    refactor: modify home page carousel width

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,6 +4,7 @@ import { Carousel } from 'antd';
 import DiscountBanner from '@/components/DiscountBanner/DiscountBanner';
 import { useDiscounts } from '@/contexts/DiscountsContext';
 import IMAGES from '@/images/Images';
+import './HomePage.scss';
 
 const HomePage: React.FC = () => {
   const { discountCodes } = useDiscounts();
@@ -11,7 +12,7 @@ const HomePage: React.FC = () => {
   return (
     <>
       <h1 className="custom-title">Welcome to our Sweet Home</h1>
-      <Carousel draggable autoplay>
+      <Carousel className="home-page__carousel" draggable autoplay>
         {discountCodes.map((code: DiscountCode, idx: number) => (
           <DiscountBanner key={code.id} discountCode={code} image={IMAGES[idx]} />
         ))}
