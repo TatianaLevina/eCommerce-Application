872ac1878f6029901baf73af77439d3eb7ac6f07
ynadt <2769286@gmail.com>
2024-06-17 12:56:25 +0300

    feat: make carousel in HomePage clickable and adjust styles

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -5,19 +5,31 @@ import DiscountBanner from '@/components/DiscountBanner/DiscountBanner';
 import { useDiscounts } from '@/contexts/DiscountsContext';
 import IMAGES from '@/images/Images';
 import './HomePage.scss';
+import { useNavigate } from 'react-router-dom';
 
 const HomePage: React.FC = () => {
   const { discountCodes } = useDiscounts();
+  const navigate = useNavigate();
+
+  const handleCarouselClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const target = event.target as HTMLElement;
+    if (!target.classList.contains('slick-dots') && !target.closest('.slick-dots')) {
+      navigate('/catalog');
+    }
+  };
 
   return (
     <>
       <h1 className="custom-title">Welcome to our Sweet Home</h1>
-      <Carousel className="home-page__carousel" draggable autoplay>
-        {discountCodes.map((code: DiscountCode, idx: number) => (
-          <DiscountBanner key={code.id} discountCode={code} image={IMAGES[idx]} />
-        ))}
-      </Carousel>
+      <div onClick={handleCarouselClick} className="carousel-wrapper">
+        <Carousel className="home-page__carousel" draggable autoplay>
+          {discountCodes.map((code: DiscountCode, idx: number) => (
+            <DiscountBanner key={code.id} discountCode={code} image={IMAGES[idx]} />
+          ))}
+        </Carousel>
+      </div>
     </>
   );
 };
+
 export default HomePage;
